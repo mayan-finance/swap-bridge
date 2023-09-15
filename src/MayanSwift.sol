6c4c50cfec66d160cc4b225dce3f146ee0192954
real <real@mayan.finance>
2023-09-15 23:24:33 +0300

    separate fulfill msg

--- src/MayanSwift.sol
+++ src/MayanSwift.sol
@@ -19,6 +19,7 @@ contract MayanSwift {
 	using BytesLib for bytes;
 
 	IWormhole wormhole;
+	address feeCollector;
 	uint16 hqChainId;
 	bytes32 hqAddr;
 	uint8 consistencyLevel;
@@ -27,8 +28,8 @@ contract MayanSwift {
 	bool paused;
 
 	struct Order {
-		bytes32 dstAuthority;
-		uint16 dstChainId;
+		bytes32 destAuthority;
+		uint16 destChainId;
 		Status status;
 	}
 
@@ -40,8 +41,8 @@ contract MayanSwift {
 		bytes32 tokenOut;
 		uint256 minAmountOut;
 		uint64 gasDrop;
-		bytes32 dstAddr;
-		uint16 dstChainId;
+		bytes32 destAddr;
+		uint16 destChainId;
 		bytes32 referrerAddr;
 		bytes32 nonce;
 	}
@@ -58,15 +59,31 @@ contract MayanSwift {
 		uint8 action;
 		bytes32 keyHash;
 		uint16 srcChain;
-		bytes32 trader;
 		bytes32 tokenIn;
-		uint256 amountIn;
+		uint64 amountIn;
 		bytes32 recipient;
 	}
 
-	constructor(address _wormhole, uint16 _hqChainId, bytes32 _hqAddr, uint8 _consistencyLevel) {
+	struct FulfillMsg {
+		uint8 action;
+		bytes32 keyHash;
+		uint16 destChainId;
+		bytes32 destAddr;
+		bytes32 driver;
+		bytes32 tokenOut;
+		uint64 amountPromised;
+		bytes32 referrerAddr;
+		uint8 referrerBps;
+		uint8 mayanBps;
+		uint16 srcChain;
+		bytes32 tokenIn;
+		uint64 amountIn;
+	}
+
+	constructor(address _wormhole, address _feeCollector, uint16 _hqChainId, bytes32 _hqAddr, uint8 _consistencyLevel) {
 		guardian = msg.sender;
 		wormhole = IWormhole(_wormhole);
+		feeCollector = _feeCollector;
 		hqChainId = _hqChainId;
 		hqAddr = _hqAddr;
 		consistencyLevel = _consistencyLevel;
@@ -74,7 +91,7 @@ contract MayanSwift {
 
 	mapping(bytes32 => Order) orders;
 
-	function createOrderWithEth(bytes32 tokenOut, uint64 minAmountOut, uint64 gasDrop, bytes32 dstAddr, uint8 dstChainId, bytes32 referrerAddr, bytes32 nonce, bytes32 dstAuthority) public payable {
+	function createOrderWithEth(bytes32 tokenOut, uint64 minAmountOut, uint64 gasDrop, bytes32 destAddr, uint8 destChainId, bytes32 referrerAddr, bytes32 nonce, bytes32 destAuthority) public payable returns (bytes32 keyHash) {
 		require(paused == false, 'contract is paused');
 		require(msg.value > 0, 'value is zero');
 
@@ -86,26 +103,26 @@ contract MayanSwift {
 			tokenOut: tokenOut,
 			minAmountOut: minAmountOut,
 			gasDrop: gasDrop,
-			dstAddr: dstAddr,
-			dstChainId: dstChainId,
+			destAddr: destAddr,
+			destChainId: destChainId,
 			referrerAddr: referrerAddr,
 			nonce: nonce
 		});
-		bytes32 keyHash = keccak256(encodeKey(key));
+		keyHash = keccak256(encodeKey(key));
 
-		require(dstChainId > 0, 'invalid dest chain id');
-		require(orders[keyHash].dstChainId == 0, 'duplicate key');
+		require(destChainId > 0, 'invalid dest chain id');
+		require(orders[keyHash].destChainId == 0, 'duplicate key');
 
 		orders[keyHash] = Order({
-			dstAuthority: dstAuthority,
-			dstChainId: dstChainId,
+			destAuthority: destAuthority,
+			destChainId: destChainId,
 			status: Status.CREATED
 		});
 
 		emit OrderCreated(keyHash);
 	}
 
-	function createOrderWithToken(bytes32 tokenOut, uint64 minAmountOut, uint64 gasDrop, bytes32 dstAddr, uint16 dstChainId, address tokenIn, uint256 amountIn, bytes32 referrerAddr, bytes32 nonce, bytes32 dstAuthority) public {
+	function createOrderWithToken(bytes32 tokenOut, uint64 minAmountOut, uint64 gasDrop, bytes32 destAddr, uint16 destChainId, address tokenIn, uint256 amountIn, bytes32 referrerAddr, bytes32 nonce, bytes32 destAuthority) public returns (bytes32 keyHash) {
 		require(paused == false, 'contract is paused');
 
 		uint256 balance = IERC20(tokenIn).balanceOf(address(this));
@@ -121,37 +138,63 @@ contract MayanSwift {
 			tokenOut: tokenOut,
 			minAmountOut: minAmountOut,
 			gasDrop: gasDrop,
-			dstAddr: dstAddr,
-			dstChainId: dstChainId,
+			destAddr: destAddr,
+			destChainId: destChainId,
 			referrerAddr: referrerAddr,
 			nonce: nonce
 		});
-		bytes32 keyHash = keccak256(encodeKey(key));
+		keyHash = keccak256(encodeKey(key));
 
-		require(dstChainId > 0, 'invalid dest chain id');
-		require(orders[keyHash].dstChainId == 0, 'duplicate key');
+		require(destChainId > 0, 'invalid dest chain id');
+		require(orders[keyHash].destChainId == 0, 'duplicate key');
 
 		orders[keyHash] = Order({
-			dstAuthority: dstAuthority,
-			dstChainId: dstChainId,
+			destAuthority: destAuthority,
+			destChainId: destChainId,
 			status: Status.CREATED
 		});
 
-		if (dstAuthority != bytes32(0)) {
-			orders[keyHash].dstAuthority = dstAuthority;
+		if (destAuthority != bytes32(0)) {
+			orders[keyHash].destAuthority = destAuthority;
 		}
 
 		emit OrderCreated(keyHash);
 	}
 
-	function fulfillOrder(bytes memory encodedVm) public {
+	function fulfillOrder(bytes memory encodedVm, bytes32 recepient) public payable returns (uint64 sequence) {
 		(IWormhole.VM memory vm, bool valid, string memory reason) = wormhole.parseAndVerifyVM(encodedVm);
 
 		require(valid, reason);
 		require(vm.emitterChainId == hqChainId, 'invalid HQ chain');
 		require(vm.emitterAddress == hqAddr, 'invalid HQ address');
 
-		SwiftMsg memory swift = parseSwiftPayload(vm.payload);
+		FulfillMsg memory fulfillMsg = parseFulfillPayload(vm.payload);
+
+		require(fulfillMsg.destChainId == wormhole.chainId(), 'wrong chain id');
+		require(truncateAddress(fulfillMsg.driver) == msg.sender, 'invalid driver');
+
+		Order memory order = orders[fulfillMsg.keyHash];
+		require(order.status == Status.CREATED, 'invalid order status');
+		order.status = Status.FULFILLED;
+
+		makePayments(fulfillMsg);
+
+		SwiftMsg memory unlockMsg = SwiftMsg({
+			action: 2,
+			keyHash: fulfillMsg.keyHash,
+			srcChain: fulfillMsg.srcChain,
+			tokenIn: fulfillMsg.tokenIn,
+			amountIn: fulfillMsg.amountIn,
+			recipient: recepient
+		});
+
+		bytes memory encoded = encodeSwiftMsg(unlockMsg);
+
+		sequence = wormhole.publishMessage{
+			value : msg.value
+		}(0, encoded, consistencyLevel);
+
+		emit OrderFulfilled(fulfillMsg.keyHash);
 	}
 
 	function unlockOrder(bytes memory encodedVm) public {
@@ -162,12 +205,12 @@ contract MayanSwift {
 		SwiftMsg memory swift = parseSwiftPayload(vm.payload);
 		Order memory order = orders[swift.keyHash];
 
-		require(vm.emitterChainId == order.dstChainId, 'invalid emitter chain');
+		require(vm.emitterChainId == order.destChainId, 'invalid emitter chain');
 
-		if (order.dstChainId == hqChainId) {
+		if (order.destChainId == hqChainId) {
 			require(vm.emitterAddress == hqAddr, 'invalid emitter address');
-		} else if (order.dstAuthority != bytes32(0)) {
-			require(vm.emitterAddress == order.dstAuthority, 'invalid emitter address');
+		} else if (order.destAuthority != bytes32(0)) {
+			require(vm.emitterAddress == order.destAuthority, 'invalid emitter address');
 		} else {
 			require(truncateAddress(vm.emitterAddress) == address(this), 'invalid emitter address');
 		}
@@ -180,10 +223,18 @@ contract MayanSwift {
 		
 		address recipient = truncateAddress(swift.recipient);
 		address tokenIn = truncateAddress(swift.tokenIn);
+		uint8 decimals;
 		if (tokenIn == address(0)) {
-			payable(recipient).transfer(swift.amountIn);
+			decimals = 18;
 		} else {
-			IERC20(tokenIn).safeTransfer(recipient, swift.amountIn);
+			decimals = decimalsOf(tokenIn);
+		}
+
+		uint256 amountIn = deNormalizeAmount(swift.amountIn, decimals);
+		if (tokenIn == address(0)) {
+			payable(recipient).transfer(amountIn);
+		} else {
+			IERC20(tokenIn).safeTransfer(recipient, amountIn);
 		}
 		
 		emit OrderUnlocked(swift.keyHash);
@@ -197,12 +248,12 @@ contract MayanSwift {
 		SwiftMsg memory swift = parseSwiftPayload(vm.payload);
 		Order memory order = orders[swift.keyHash];
 
-		require(vm.emitterChainId == order.dstChainId, 'invalid emitter chain');
+		require(vm.emitterChainId == order.destChainId, 'invalid emitter chain');
 
-		if (order.dstChainId == hqChainId) {
+		if (order.destChainId == hqChainId) {
 			require(vm.emitterAddress == hqAddr, 'invalid emitter address');
-		} else if (order.dstAuthority != bytes32(0)) {
-			require(vm.emitterAddress == order.dstAuthority, 'invalid emitter address');
+		} else if (order.destAuthority != bytes32(0)) {
+			require(vm.emitterAddress == order.destAuthority, 'invalid emitter address');
 		} else {
 			require(truncateAddress(vm.emitterAddress) == address(this), 'invalid emitter address');
 		}
@@ -213,20 +264,28 @@ contract MayanSwift {
 		
 		orders[swift.keyHash].status = Status.CANCEL_COMPLETED;
 		
-		address trader = truncateAddress(swift.trader);
+		address trader = truncateAddress(swift.recipient);
 		require(trader == msg.sender, 'invalid canceller');
 
 		address tokenIn = truncateAddress(swift.tokenIn);
+		uint8 decimals;
 		if (tokenIn == address(0)) {
-			payable(trader).transfer(swift.amountIn);
+			decimals = 18;
 		} else {
-			IERC20(tokenIn).safeTransfer(trader, swift.amountIn);
+			decimals = decimalsOf(tokenIn);
+		}
+
+		uint256 amountIn = deNormalizeAmount(swift.amountIn, decimals);
+		if (tokenIn == address(0)) {
+			payable(trader).transfer(amountIn);
+		} else {
+			IERC20(tokenIn).safeTransfer(trader, amountIn);
 		}
 
 		emit CancelCompleted(swift.keyHash);
 	}
 
-	function InitiateCancelOrder(uint16 srcChainId, address tokenIn, uint256 amountIn, bytes32 tokenOut, uint256 minAmountOut, uint64 gasDrop, bytes32 dstAddr, uint16 dstChainId, bytes32 referrerAddr, bytes32 nonce) public payable returns (uint64 sequence) {
+	function InitiateCancelOrder(uint16 srcChainId, address tokenIn, uint256 amountIn, bytes32 tokenOut, uint256 minAmountOut, uint64 gasDrop, bytes32 destAddr, uint16 destChainId, bytes32 referrerAddr, bytes32 nonce) public payable returns (uint64 sequence) {
 		Key memory key = Key({
 			trader: msg.sender,
 			srcChainId: srcChainId,
@@ -235,8 +294,8 @@ contract MayanSwift {
 			tokenOut: tokenOut,
 			minAmountOut: minAmountOut,
 			gasDrop: gasDrop,
-			dstAddr: dstAddr,
-			dstChainId: dstChainId,
+			destAddr: destAddr,
+			destChainId: destChainId,
 			referrerAddr: referrerAddr,
 			nonce: nonce
 		});
@@ -264,30 +323,118 @@ contract MayanSwift {
 		emit CancelInitiated(keyHash);
 	}
 
+	function makePayments(FulfillMsg memory fulfillMsg) internal {
+		address tokenOut = truncateAddress(fulfillMsg.tokenOut);
+		uint8 decimals;
+		if (tokenOut == address(0)) {
+			decimals = 18;
+		} else {
+			decimals = decimalsOf(tokenOut);
+		}
+
+		uint256 amountPromised = deNormalizeAmount(fulfillMsg.amountIn, decimals);
+		address referrerAddr = truncateAddress(fulfillMsg.referrerAddr);
+		
+		uint256 amountReferrer = 0;
+		if (referrerAddr != address(0) && fulfillMsg.referrerBps != 0) {
+			amountReferrer = amountPromised * fulfillMsg.referrerBps / 10000;
+		}
+
+		uint256 amountMayan = 0;
+		if (fulfillMsg.mayanBps != 0) {
+			amountMayan = amountPromised * fulfillMsg.mayanBps / 10000;
+		}
+
+		address destAddr = truncateAddress(fulfillMsg.destAddr);
+		if (tokenOut == address(0)) {
+			if (amountReferrer > 0) {
+				payable(referrerAddr).transfer(amountReferrer);
+			}
+			if (amountMayan > 0) {
+				payable(feeCollector).transfer(amountMayan);
+			}
+			payable(destAddr).transfer(amountPromised - amountReferrer - amountMayan);
+		} else {
+			if (amountReferrer > 0) {
+				IERC20(tokenOut).safeTransferFrom(msg.sender, referrerAddr, amountReferrer);
+			}
+			if (amountMayan > 0) {
+				IERC20(tokenOut).safeTransferFrom(msg.sender, feeCollector, amountMayan);
+			}
+			IERC20(tokenOut).safeTransferFrom(msg.sender, destAddr, amountPromised - amountReferrer - amountMayan);
+		}
+	}
+
+	function parseFulfillPayload(bytes memory encoded) public pure returns (FulfillMsg memory fulfillMsg) {
+		uint index = 0;
+
+		fulfillMsg.action = encoded.toUint8(index);
+		index += 1;
+
+		// actions: 1 = fulfill, 2 = unlock 3 = init cancel
+		require(fulfillMsg.action == 1, 'invalid action');
+
+		fulfillMsg.keyHash = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.destChainId = encoded.toUint16(index);
+		index += 2;
+
+		fulfillMsg.destAddr = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.driver = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.tokenOut = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.amountPromised = encoded.toUint64(index);
+		index += 8;
+
+		fulfillMsg.referrerAddr = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.referrerBps = encoded.toUint8(index);
+		index += 1;
+
+		fulfillMsg.mayanBps = encoded.toUint8(index);
+		index += 1;
+
+		fulfillMsg.srcChain = encoded.toUint16(index);
+		index += 2;
+
+		fulfillMsg.tokenIn = encoded.toBytes32(index);
+		index += 32;
+
+		fulfillMsg.amountIn = encoded.toUint64(index);
+		index += 32;
+
+		require(encoded.length == index, 'invalid msg lenght');
+	}
+
 	function parseSwiftPayload(bytes memory encoded) public pure returns (SwiftMsg memory swiftMsg) {
-        uint index = 0;
+		uint index = 0;
 
 		swiftMsg.action = encoded.toUint8(index);
 		index += 1;
 
-		// actions: 1 = fulfill, 2 = unlock 3 = cancel
-		require(swiftMsg.action > 0 && swiftMsg.action < 4, 'invalid action');
+		// actions: 1 = fulfill, 2 = unlock 3 = init cancel
+		require(swiftMsg.action == 2 || swiftMsg.action == 3, 'invalid action');
 
-        swiftMsg.keyHash = encoded.toBytes32(index);
-        index += 32;
-
-		swiftMsg.trader = encoded.toBytes32(index);
+		swiftMsg.keyHash = encoded.toBytes32(index);
 		index += 32;
 
-        swiftMsg.srcChain = encoded.toUint16(index);
-        index += 2;
+		swiftMsg.srcChain = encoded.toUint16(index);
+		index += 2;
 
-		if (swiftMsg.action == 2) {
-			swiftMsg.recipient = encoded.toBytes32(index);
-			index += 32;
-		}
+		swiftMsg.tokenIn = encoded.toBytes32(index);
+		index += 32;
 
-        require(encoded.length == index, 'invalid swift msg');
+		swiftMsg.recipient = encoded.toBytes32(index);
+		index += 32;
+
+		require(encoded.length == index, 'invalid msg lenght');
 	}
 
 	function encodeKey(Key memory key) internal pure returns (bytes memory encoded) {
@@ -299,16 +446,45 @@ contract MayanSwift {
 			key.tokenOut,
 			key.minAmountOut,
 			key.gasDrop,
-			key.dstAddr,
-			key.dstChainId,
+			key.destAddr,
+			key.destChainId,
 			key.referrerAddr,
 			key.nonce
+		);
+	}
+
+	function encodeSwiftMsg(SwiftMsg memory swiftMsg) internal pure returns (bytes memory encoded) {
+		encoded = abi.encodePacked(
+			swiftMsg.action,
+			swiftMsg.keyHash,
+			swiftMsg.srcChain,
+			swiftMsg.tokenIn,
+			swiftMsg.recipient
 		);
 	}
 
 	function truncateAddress(bytes32 b) internal pure returns (address) {
 		require(bytes12(b) == 0, 'invalid EVM address');
 		return address(uint160(uint256(b)));
+	}
+
+	function decimalsOf(address token) internal view returns(uint8) {
+		(,bytes memory queriedDecimals) = token.staticcall(abi.encodeWithSignature('decimals()'));
+		return abi.decode(queriedDecimals, (uint8));
+	}
+
+	function normalizeAmount(uint256 amount, uint8 decimals) internal pure returns(uint256) {
+		if (decimals > 8) {
+			amount /= 10 ** (decimals - 8);
+		}
+		return amount;
+	}
+
+	function deNormalizeAmount(uint256 amount, uint8 decimals) internal pure returns(uint256) {
+		if (decimals > 8) {
+			amount *= 10 ** (decimals - 8);
+		}
+		return amount;
 	}
 
 	function setPause(bool _pause) public {
