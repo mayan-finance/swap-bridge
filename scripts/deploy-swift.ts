f8b2a8fd2149bc65e8c444a9e823be20e42a7566
real <real@mayan.finance>
2024-03-13 22:19:08 +0300

    add create-order-sig script

--- scripts/deploy-swift.ts
+++ scripts/deploy-swift.ts
@@ -4,12 +4,16 @@ import { base58_to_binary } from "base58-js";
 import { PublicKey } from "@solana/web3.js";
 
 async function main(wormholeAddr: string, tokenBridgeAddr: string) {
+	const [operator] = await ethers.getSigners();
+	const FeeManager = await ethers.getContractFactory("FeeManager");
+	const feeManager = await FeeManager.deploy(operator.address);
+
 	const tokenBridge = await ethers.getContractAt("ITokenBridge", tokenBridgeAddr);
 	const finality = await tokenBridge.finality();
 
 	const MayanSwift = await ethers.getContractFactory("MayanSwift");
 	// constructor(address _wormhole, address _feeCollector, uint16 _auctionChainId, bytes32 _auctionAddr, bytes32 _solanaEmitter, uint8 _consistencyLevel) {
-	const mayanSwift = await MayanSwift.deploy(wormholeAddr, '0xeE1A58EafE1977A3D2ae59E2897Bb815054f6D58', 1, getEmitterPda('HgZHDPTnyCEhkxE5gx5GUrfYf3WU4ecp3Ky6i8YdtWFn'), getEmitterPda('7oJsHfUYjRNomVEM9uZtMWoiSJEPqrbHTejbpg1U23oS'), finality);
+	const mayanSwift = await MayanSwift.deploy(wormholeAddr, feeManager.address, 1, getEmitterPda('4oUq8HocfbPUpvu1j5ZVbLcoak7DFz2CLK3f91qUuQzH'), getEmitterPda('5vBpQGxxnzjhv3FNFVpVmGWsUhhNFu4xTbyGs3W2Sbbx'), finality);
 
 	const deployed = await mayanSwift.deployed();
 
