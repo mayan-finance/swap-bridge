b6012baff7b0cd4ad17a3ceac0f92b12d82c3537
real <real@mayan.finance>
2024-07-10 01:48:41 +0300

    update fee manager

--- src/FeeManager.sol
+++ src/FeeManager.sol
@@ -10,9 +10,13 @@ contract FeeManager is IFeeManager {
 	using SafeERC20 for IERC20;
 
 	address public operator;
+	address public nextOperator;
+	uint8 public baseBps;
+	address public treasury;
 
-	constructor(address _operator) {
+	constructor(address _operator, uint8 _baseBps) {
 		operator = _operator;
+		baseBps = _baseBps;
 	}
 	
 	function calcProtocolBps(
@@ -21,17 +25,30 @@ contract FeeManager is IFeeManager {
 		bytes32 tokenOut,
 		uint16 destChain,
 		uint8 referrerBps
-	) external view override returns (uint8 protocolBps) {
-		protocolBps = referrerBps;
+	) external view override returns (uint8) {
+		if (referrerBps > baseBps) {
+			return referrerBps;
+		} else {
+			return baseBps;
+		}
 	}
 
 	function feeCollector() external view override returns (address) {
-		return address(this);
+		if (treasury != address(0)) {
+			return treasury;
+		} else {
+			return address(this);
+		}
 	}
 
-	function setOperator(address _operator) external {
+	function changeOperator(address _nextOperator) external {
 		require(msg.sender == operator, 'only operator');
-		operator = _operator;
+		nextOperator = _nextOperator;
+	}	
+
+	function claimOperator() external {
+		require(msg.sender == nextOperator, 'only next operator');
+		operator = nextOperator;
 	}
 
 	function sweepToken(address token, uint256 amount, address to) public {
@@ -43,6 +60,16 @@ contract FeeManager is IFeeManager {
 		require(msg.sender == operator, 'only operator');
 		require(to != address(0), 'transfer to the zero address');
 		to.transfer(amount);
+	}
+
+	function setBaseBps(uint8 _baseBps) external {
+		require(msg.sender == operator, 'only operator');
+		baseBps = _baseBps;
+	}
+
+	function setTreasury(address _treasury) external {
+		require(msg.sender == operator, 'only operator');
+		treasury = _treasury;
 	}
 
 	receive() external payable {}
