7a23420a4e22e5392307da9334ac5cbb909ad5c5
Mohammad Zolfaghari <zidom72@gmail.com>
2024-12-04 17:25:07 +0330

    fix: okx contract name

--- src/OkxHelper.sol
+++ src/OkxHelper.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract MayanOkxForwarder {
+contract OkxHelper {
     using SafeERC20 for IERC20;
 
     address public guardian;
