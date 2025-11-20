f37b746cccb5e9d193fe11c2ed1c8a04d970e2e9
real <real@mayan.finance>
2025-11-20 23:46:59 +0000

    rename to monochain

--- src/MonoChainSwap.sol
+++ src/MonoChainSwap.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract OnchainSwap {
+contract MonoChainSwap {
 	using SafeERC20 for IERC20;
 
 	error Unauthorized();
